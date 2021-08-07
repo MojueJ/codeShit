3d6ed0ed05bbce2aab74dbc54ede51e59162e155
MojueJ <wzzlok2016@gmail.com>
2021-08-07 09:57:25 +0800

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,25 @@
+Python 3.7.3 (v3.7.3:ef4ec6ed12, Mar 25 2019, 16:52:21) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> fiv
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    fiv
+NameError: name 'fiv' is not defined
+>>> fib
+Traceback (most recent call last):
+  File "<pyshell#1>", line 1, in <module>
+    fib
+NameError: name 'fib' is not defined
+>>> f=fib
+Traceback (most recent call last):
+  File "<pyshell#2>", line 1, in <module>
+    f=fib
+NameError: name 'fib' is not defined
+>>> exit
+Use exit() or Ctrl-D (i.e. EOF) to exit
+>>> 
+KeyboardInterrupt
+>>> list(range(5))
+[0, 1, 2, 3, 4]
+>>> 
